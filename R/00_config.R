d5a8e71b6605b4f3b75ba01aca3d5156bda6b7be
pvmyakina <pvmyakina@gmail.com>
2025-12-19 22:40:41 +0300

    add configs

---
+++ R/00_config.R
@@ -0,0 +1,7 @@
+dir_raw   <- "data_raw"
+dir_ocr   <- "data_ocr"
+dir_clean <- "data_clean"
+dir_out_t <- file.path("outputs", "tables")
+dir_out_f <- file.path("outputs", "figures")
+tess_lang <- "eng"
+ud_lang <- "english-ewt"
